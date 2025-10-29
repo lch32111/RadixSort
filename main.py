206d75b0a32e72aa63af5c720b6dd15d6f82472b
Chanhaeng <lch32111@gmail.com>
2025-10-29 23:36:37 +0900

    start to write main radix sort codes

--- main.py
+++ main.py
@@ -8,3 +8,12 @@ if __name__ == "__main__":
         enable_debug_layers=True,
         compiler_options={"include_paths": [HERE_DIR]},
     )
+
+    count_pass_kernel = device.create_compute_kernel(
+        device.load_program("radix.slang", ["count_pass"])
+    )
+    count_reduce_pass_kernel = device.create_compute_kernel(
+        device.load_program("radix.slang", ["count_reduce_pass"])
+    )
+    print(count_pass_kernel)
+    print(count_reduce_pass_kernel)
